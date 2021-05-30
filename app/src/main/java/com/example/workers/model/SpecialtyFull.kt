1792a2598167f2544553aaa9021ecead66b3f3b2
al-metal <al-metal@bk.ru>
2021-05-31 00:31:41 +0400

    Конвертер БД

--- app/src/main/java/com/example/workers/model/SpecialtyFull.kt
+++ app/src/main/java/com/example/workers/model/SpecialtyFull.kt
@@ -2,6 +2,8 @@ package com.example.workers.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 
 data class SpecialtyFull(
     var specialty_id: Int? = null,
@@ -10,13 +12,15 @@ data class SpecialtyFull(
 )
 
 @Entity
+
 data class UserRoom(
     @PrimaryKey
     var id: Long? = null,
     var f_name: String? = null,
     var l_name: String? = null,
     var birthday: String? = null,
-    var avatr_url: String? = null
+    var avatr_url: String? = null,
+    var specList: List<Int>? = null
 )
 
 @Entity
@@ -25,3 +29,17 @@ data class SpecialtyRoom(
     var specialty_id: Int? = null,
     var name: String? = null
 )
+
+class SpecialtyConverter {
+    @TypeConverter
+    fun fromSpecList(specList: List<Int>?): String? {
+        val list = specList?.map { it.toString() }
+        return list?.joinToString { "," }
+    }
+
+    @TypeConverter
+    fun toSpecList(data: String): List<Int>? {
+        val list = data.split(",")
+        return list.map { it.toInt() }
+    }
+}
--- app/src/main/java/com/example/workers/model/databaseWorkers.kt
+++ app/src/main/java/com/example/workers/model/databaseWorkers.kt
@@ -2,11 +2,13 @@ package com.example.workers.model
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(
     entities = [UserRoom::class,
         SpecialtyRoom::class], version = 1
 )
+@TypeConverters(SpecialtyConverter::class)
 abstract class databaseWorkers : RoomDatabase() {
 
     abstract fun userDao(): UserDao
